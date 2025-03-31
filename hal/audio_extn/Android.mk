2924868e8ceba6af9b562cb66ff4d7bf98e94532
EncryptedCicada <dev.varnit@gmail.com>
2025-03-31 21:42:00 +0530

    Revert changing library

--- hal/audio_extn/Android.mk
+++ hal/audio_extn/Android.mk
@@ -336,8 +336,13 @@ LOCAL_MODULE := libspkrprot
 LOCAL_MODULE_OWNER := third_party
 LOCAL_VENDOR_MODULE := true
 
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_MOTOROLA_CIRRUS)),true)
+LOCAL_SRC_FILES := \
+        mot_sp.c
+else
 LOCAL_SRC_FILES:= \
         spkr_protection.c
+endif
 
 LOCAL_CFLAGS += \
     -Wall \
@@ -410,13 +415,8 @@ LOCAL_MODULE := libcirrusspkrprot
 LOCAL_MODULE_OWNER := third_party
 LOCAL_VENDOR_MODULE := true
 
-ifeq ($(strip $(AUDIO_FEATURE_ENABLED_MOTOROLA_CIRRUS)),true)
-LOCAL_SRC_FILES := \
-        mot_sp.c
-else
 LOCAL_SRC_FILES:= \
         cirrus_playback.c
-endif
 
 LOCAL_CFLAGS += \
     -Wall \
