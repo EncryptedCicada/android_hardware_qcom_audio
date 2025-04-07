f8933e69d04deae0adfbdae4204513975e052a2c
EncryptedCicada <dev.varnit@gmail.com>
2025-04-07 15:39:40 +0530

    Revert "TO REVERT: Debug changes to see why our library is not used during runtime."
    This reverts commit 63a94daba067b56502b09ff4ad803b0ad3bb4c19.

--- hal/audio_extn/audio_extn.c
+++ hal/audio_extn/audio_extn.c
@@ -1895,16 +1895,12 @@ void spkr_prot_feature_init(bool is_feature_enabled)
     if (is_feature_enabled) {
         // dlopen lib
 #if LINUX_ENABLED
-        ALOGI("%s: LINUX_ENABLED is set to true somewhere.", __func__);
         spkr_prot_lib_handle = dlopen(SPKR_PROT_LIB_PATH, RTLD_NOW);
 #else
-        ALOGI("%s: LINUX_ENABLED is set to false.", __func__);
         if ((vendor_enhanced_info & 0x3) == 0x0) // Pure AOSP
             spkr_prot_lib_handle = dlopen(CIRRUS_SPKR_PROT_LIB_PATH, RTLD_NOW);
-            ALOGI("%s: spkr_prot_lib_handle is set to: %s", __func__, CIRRUS_SPKR_PROT_LIB_PATH);
         else
             spkr_prot_lib_handle = dlopen(SPKR_PROT_LIB_PATH, RTLD_NOW);
-            ALOGI("%s: spkr_prot_lib_handle is set to: %s", __func__, SPKR_PROT_LIB_PATH);
 #endif
 
         if (spkr_prot_lib_handle == NULL) {
