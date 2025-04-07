21ec666320ad7adf5902866ad61e5c613dc8679a
EncryptedCicada <dev.varnit@gmail.com>
2025-04-07 12:44:33 +0530

    Revert changes to platform code.

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -51,10 +51,6 @@
 #include "sound/msmcal-hwdep.h"
 #include <dirent.h>
 
-#ifdef MOTOROLA_AUDIO
-#include "mot_sp.h"
-#endif
-
 #include "../audio_hw_lvacfs.h"
 #include "../audio_hw_lvimfs.h"
 #ifdef DYNAMIC_LOG_ENABLED
@@ -419,9 +415,6 @@ struct platform_data {
     struct listnode custom_mtmx_in_params_list;
     struct power_mode_cfg power_mode_cfg[SND_DEVICE_MAX];
     struct island_cfg island_cfg[SND_DEVICE_MAX];
-#ifdef MOTOROLA_AUDIO
-    void *mot_handle;
-#endif
 };
 
 struct  spkr_device_chmap {
@@ -4356,11 +4349,6 @@ acdb_init_fail:
     if (property_get_bool("persist.vendor.audio.apptype.multirec.enabled", false))
         my_data->use_generic_handset = true;
 
-#ifdef MOTOROLA_AUDIO
-    /* Motorola customization for berlin audio */
-    my_data->mot_handle = audio_extn_mot_spkr_prot_init(my_data->adev);
-#endif
-
     /* Initialize keep alive for HDMI/loopback silence */
     audio_extn_keep_alive_init(adev);
 
@@ -4571,11 +4559,6 @@ void platform_deinit(void *platform)
     struct external_specific_device *ext_dev;
     struct app_type_entry *ap;
     struct listnode *node;
-
-#ifdef MOTOROLA_AUDIO
-    /* Motorola customization for berlin audio */
-    audio_extn_mot_spkr_prot_deinit(my_data->mot_handle);
-#endif
 
     audio_extn_keep_alive_deinit();
     platform_reset_edid_info(my_data);
